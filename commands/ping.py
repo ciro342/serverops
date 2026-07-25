8f22ba461774c9a53377673f554dcbffac1d9060
ciro342 <ciroprg@gmail.com>
2026-07-24 19:38:43 -0500

    add rich in ping.py

--- commands/ping.py
+++ commands/ping.py
@@ -4,7 +4,9 @@ import yaml
 import subprocess
 import logging
 import core.logger
+from rich.console import Console
 
+console=Console()
 server_config="config/servers.yml"
 app_ping=typer.Typer()
 @app_ping.command()
@@ -22,10 +24,12 @@ def choose(name:str):
             print(ip)
             resul=subprocess.run(["ping","-c","3",f"{ip}"],capture_output=True,text=True)
             if resul.returncode==0:
-                print(f"Si hay conexion con {name}")
+                #print(f"Si hay conexion con {name}")
+                console.print(f"[bold green]✓ Si hay conexion para {name}.[/bold green]")
                 logging.info(f"Si hay conexion con ping para : {name}")
             else:
-                print(f"No hay conexion para {name}")
+                console.print(f"[bold red]x No hay conexion para {name}.[/bold red]")
+                #print(f"No hay conexion para {name}")
                 logging.error(f"No hay conexion para {name}")
     except FileExistsError:
         print("NO existe el archivo ")
